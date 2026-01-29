1956222e79682bfc2efdbe74a9563168cc5f2235
Michael <michael.ajanaku@xprizo.com>
2026-01-30 00:45:12 +0100

    update

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -14,15 +14,16 @@ import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dtos/create-transaction.dto';
 import { RequireKyc } from 'src/kyc/kyc.decorator';
+import { KycGuard } from 'src/kyc/guards/kyc.guard';
 // import { KycGuard } from 'src/kyc/guards/kyc.guard';
 
 @UseGuards(JwtAuthGuard)
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly service: TransactionsService) {}
-  // @UseGuards(KycGuard)
-  @Post()
+  @UseGuards(KycGuard)
   @RequireKyc(1)
+  @Post()
   create(@Req() req, @Body() dto: CreateTransactionDto) {
     return this.service.create(req.user.id, dto);
   }
